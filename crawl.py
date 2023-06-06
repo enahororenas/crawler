3c8812e71fa9bc50536818959f6424dd1956d0ce
enahoroenas <orieroenahoro@yahoo.com>
2023-06-06 01:42:59 -0700

    added queue

--- crawl.py
+++ crawl.py
@@ -5,12 +5,14 @@ import re
 from pprint import pprint
 from urllib.parse import urljoin
 import logging
+from collections import deque
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
 class Crawler:
     def __init__(self, urls=[]):
         self.visited_urls = set()
-        self.urls_to_visit = urls
+        self.urls_to_visit = deque(urls)
         
     def download_url(self, url): return requests.get(url).text
     
@@ -32,7 +34,7 @@ class Crawler:
     
     def run(self):
         while self.urls_to_visit:
-            url = self.urls_to_visit.pop(0)
+            url = self.urls_to_visit.popleft()
             if url not in self.visited_urls:
                 logging.info('TRY: {}'.format(url))
                 try: self.crawl(url)
@@ -40,10 +42,10 @@ class Crawler:
             
     def crawl(self,url):
         logging.info('Crawling: {}'.format(url))
-        html = self.download_url(url)        
-        for url in self.bfs(url, html):
-            if url and url not in self.visited_urls : self.urls_to_visit.append(url)
+        html = self.download_url(url) 
+        for next_url in self.bfs(url, html):
+            if next_url and next_url.startswith(url): self.urls_to_visit.append(next_url)
         
 if __name__ == '__main__':
-    #cw = Crawler(urls=['https://www.google.com'])
+    #cw = Crawler(urls=['http://www.cnn.com/'])
     #cw.run()
